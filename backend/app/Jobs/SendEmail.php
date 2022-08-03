0d424ae439d35528a2c9ff57f26281ace62dae06
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2022-08-03 19:01:49 +0530

    modifing as per need

--- backend/app/Jobs/SendEmail.php
+++ backend/app/Jobs/SendEmail.php
@@ -23,7 +23,6 @@
          */
         public function __construct(array $message)
         {
-//            $this->email = $email;
             $this->message = $message;
         }
 
@@ -34,26 +33,13 @@
          */
         public function handle()
         {
-
             for ($i = 0; $i < count($this->message); $i++) {
                 Mail::html($this->message[$i]['message'], function ($message) use ($i) {
                     logger($this->message[$i]['message'].'Sending email to '.$this->message[$i]['email']." at ".date('Y-m-d H:i:s'));
                     $message->to($this->message[$i]['email']);
                 });
 
-                Mail::failures(function ($message) {
-                    logger("Failed to send email to ".$this->message[$i]['email']." at ".date('Y-m-d H:i:s'));
-                });
+
             }
-//            for ($i = 0; $i < count($this->data); $i++) {
-//                $email = $this->data[$i]['email'];
-//                $name = $this->data[$i]['name'];
-//                logger()->info('Message for '.$email.': '.$this->message);
-//                usleep(100);
-//                $msg = sprintf($this->message, $name, Carbon::now()->toDateString());
-//                Mail::html($msg, function ($message) use ($name, $email) {
-//                    $message->to($email, $name)->subject('Test email');
-//                });
-//            }
         }
     }
